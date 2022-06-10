6b8ebab2ee6122a8defbc4c675a27f849f01d06c
Aleksandr Butukhanov <avbutukhanov@gmail.com>
2022-06-10 22:27:48 +0800

    adding tests

--- src/test/java/ru/netology/domain/PosterManagerTest.java
+++ src/test/java/ru/netology/domain/PosterManagerTest.java
@@ -55,4 +55,46 @@ public class PosterManagerTest {
 
         Assertions.assertArrayEquals(expected, actual);
     }
+
+    @Test
+    public void test2(){
+        PosterManager manager = new PosterManager(5);
+
+        manager.add (first);
+        manager.add(second);
+        manager.add(third);
+        manager.add(fourth);
+        manager.add(fifth);
+        manager.add(sixth);
+        manager.add(seventh);
+        manager.add(eigthth);
+        manager.add(ninth);
+        manager.add(tenth);
+
+        Poster[] actual = manager.findLast();
+        Poster[] expected = {tenth, ninth, eigthth, seventh, sixth};
+
+        Assertions.assertArrayEquals(expected, actual);
+    }
+
+    @Test
+    public void test3(){
+        PosterManager manager = new PosterManager(50);
+
+        manager.add (first);
+        manager.add(second);
+        manager.add(third);
+        manager.add(fourth);
+        manager.add(fifth);
+        manager.add(sixth);
+        manager.add(seventh);
+        manager.add(eigthth);
+        manager.add(ninth);
+        manager.add(tenth);
+
+        Poster[] actual = manager.findLast();
+        Poster[] expected = {tenth, ninth, eigthth, seventh, sixth, fifth, fourth, third, second, first};
+
+        Assertions.assertArrayEquals(expected, actual);
+    }
 }
